7c658f01fa493e55546e990b198cce5303b8c11e
Vasilev-Stefan <sbasilev@gmail.com>
2025-09-21 18:52:24 +0300

    show home page and add css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import http from 'http';
+import homeHtml from './home.html.js'
+import siteCSS from './site.css.js'
 
 const server = http.createServer((req, res) => {
-    res.write('Hello World!');
+
+    if(req.url === '/'){
+        res.writeHead(200, {
+            "content-type": 'text/html'
+        })
+
+        res.write(homeHtml);
+    } else if(req.url === '/styles/site.css'){
+        res.writeHead(200, {
+            "content-type": 'text/css',
+        });
+        res.write(siteCSS)
+    }
+
 
     res.end()
 });
 
 server.listen(5000);
 
-console.log('Server is listening on http://localhost:5000...')
+console.log('Server is listening on http://localhost:5000...') 
