9b3a73aa7205f0545c8dc7b3f7e4df4585bb0ec5
Vasilev-Stefan <sbasilev@gmail.com>
2025-09-25 19:07:54 +0300

    added edit cat view

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import fs from 'fs/promises';
 const server = http.createServer(async (req, res) => {
 
     if(req.url === '/'){
-        // const homeHtml = await fs.readFile('./src/views/home/index.html', {encoding: 'utf-8'})
-
         res.writeHead(200, {
             "content-type": 'text/html'
         })
@@ -35,6 +33,9 @@ const server = http.createServer(async (req, res) => {
         })
 
         res.write(addCatHtml);
+    } else if (req.url.includes('/cats/edit')) {
+        const id = req.url.split('/').at(3)
+        res.write(await displayEditView(id))
     }
 
 
@@ -60,7 +61,7 @@ async function createHomeHTML () {
         <p><span>Breed: </span>${cat.breed}</p>
         <p><span>Description: </span>${cat.description}</p>
         <ul class="buttons">
-            <li class="btn edit"><a href="">Change Info</a></li>
+            <li class="btn edit"><a href="/cats/edit/${cat.id}">Change Info</a></li>
             <li class="btn delete"><a href="">New Home</a></li>
         </ul>
     </li>
@@ -68,6 +69,37 @@ async function createHomeHTML () {
     homeHtml = homeHtml.replaceAll('{{cats}}', catsHtml.join('\n'))
     return homeHtml
 
+}
+
+async function displayEditView (id) {
+    const databaseString = await fs.readFile('./src/db.json', {encoding: 'utf-8'})
+    const database = JSON.parse(databaseString); 
+    const myCat = database.cats.find(cat => cat.id == id)
+
+    if(!myCat){
+        //implement error page
+        return `<h1>This cat is no longer available</h1>`
+    }
+    const form = `
+    <form action="#" method="" class="cat-form">
+        <h2>Edit Cat</h2>
+        <label for="name">Name</label>
+        <input type="text" id="name" value="${myCat.name}">
+        <label for="description">Description</label>
+        <textarea id="description">${myCat.description}</textarea>
+        <label for="image">Image</label>
+        <input type="text" id="image" value="${myCat.imageUrl}">
+        <label for="group">Breed</label>
+        <select id="group">
+            <option value="${myCat.breed}">${myCat.breed}</option>
+        </select>
+        <button>Edit Cat</button>
+    </form>
+    `
+    const html = await fs.readFile('./src/views/editCat.html', {encoding: "utf-8"});
+    const finalHtml = html.replaceAll('{{form}}', form)
+
+    return finalHtml
 }
 
 console.log('Server is listening on http://localhost:5000...') 
